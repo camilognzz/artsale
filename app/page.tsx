9bed1a06319c378d3c9868a70837785aa2cacaf7
Camilo Gonzalez <camilogs2218@gmail.com>
2024-09-06 21:15:41 -0500

    Page principal terminada

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,46 @@
 import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
 import { Image } from "@nextui-org/image";
-import { Button, ButtonGroup } from "@nextui-org/button";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-
+import { Button } from "@nextui-org/button";
 
 export default function Home() {
-    const bg = {
-      background: "url('/images/bg3.png')no-repeat",
-      backgroundPosition: "right"
-    }
-
-      return (
-            <div className="container mx-auto md:px-20">
-              <div className="grid md:grid-cols-2 gap-8 items-center">
-                <div className="image p-4">
-                  <Link href={"/"}>
-                    <Image
-                      isBlurred
-                      width={650}
-                      height={450}
-                      src="/images/page-principal.png"
-                      alt="Principal Image"
-                      className="m-5 w-full object-cover"
-                    />
-                  </Link>
-                </div>
-                <div className="info p-4">
-                  <div className="title">
-                    <a className="text-5xl md:text-7.5xl font-bold text-gray-800 hover:text-gray-600">
-                      Descubre el Arte que Inspira
-                    </a>
-                  </div>
-                  <p className="text-gray-700 py-3">
-                    Explora una colección única de obras de arte contemporáneo,
-                    cuidadosamente seleccionadas para transformar tu espacio y
-                    estimular tu creatividad.
-                  </p>
-                  <div>
-                    <Button
-                      className="bg-gradient-to-tr text-white shadow-lg bg-cyan-900"
-                      variant="shadow"
-                    >
-                      Compra ahora
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-         
-      );
+  return (
+    <div className="container mx-auto px-4 md:px-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+        <div className="image p-4">
+          <Link href={"/"}>
+            <Image
+              isBlurred
+              width={650}
+              height={450}
+              src="/images/page-principal.png"
+              alt="Principal Image"
+              className="m-5 w-full object-cover rounded-md"
+            />
+          </Link>
+        </div>
+        <div className="info p-4">
+          <div className="title mb-4">
+            <a className="text-2xl md:text-4xl lg:text-5xl font-bold text-gray-800 hover:text-gray-600">
+              Descubre el Arte que Inspira
+            </a>
+          </div>
+          <p className="text-sm md:text-base lg:text-lg text-gray-700 py-3">
+            Explora una colección única de obras de arte contemporáneo,
+            cuidadosamente seleccionadas para transformar tu espacio y estimular
+            tu creatividad.
+          </p>
+          <div>
+            <Button
+              className="bg-rose-600 text-white py-2 px-4 md:py-3 md:px-6"
+              size="md"
+              radius="lg"
+              variant="shadow"
+            >
+              Compra ahora
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,56 +7,25 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
-
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-  Logo,
-} from "@/components/icons";
+import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <Logo />
       <p className="font-bold text-inherit">ArtSale</p>
       <NavbarContent className="basis-1/5 sm:basis-full" justify="center">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            {/* Logo o enlace de marca */}
-          </NextLink>
+          <NextLink
+            className="flex justify-start items-center gap-1"
+            href="/"
+          ></NextLink>
         </NavbarBrand>
         {/* Menu visible en pantallas de 640px o más */}
         <ul className="hidden md:flex gap-4 justify-start ml-2">
@@ -66,8 +35,8 @@ export const Navbar = () => {
                 className={clsx(
                   linkStyles({ color: "foreground" }),
                   "data-[active=true]:text-primary data-[active=true]:font-medium",
-                  "hover:text-[#F4BC44]", // Color de hover en pantallas grandes
-                  "transition-colors duration-300" // Transición suave para el cambio de color
+                  "hover:text-[#F4BC44]",
+                  "transition-colors duration-300"
                 )}
                 color="foreground"
                 href={item.href}
@@ -78,7 +47,6 @@ export const Navbar = () => {
           ))}
         </ul>
       </NavbarContent>
-
       <NavbarContent
         className="hidden md:flex basis-1/5 sm:basis-full"
         justify="end"
@@ -87,13 +55,11 @@ export const Navbar = () => {
           <ThemeSwitch />
         </NavbarItem>
       </NavbarContent>
-
       {/* Mostrar el botón de menú hamburguesa en pantallas menores de 640px */}
       <NavbarContent className="md:hidden flex basis-1 pl-4" justify="end">
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
-
       {/* Mostrar el menú desplegable en pantallas menores de 640px */}
       <NavbarMenu className="md:hidden">
         <div className="mx-4 mt-2 flex flex-col gap-2">
